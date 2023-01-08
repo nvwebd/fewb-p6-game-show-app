e170437a5d9ef73e2cf7fca0cd54214284c20774
Nejc Vukovic <nejc.vukovic@nvwebd.com>
2023-01-08 14:43:08 +0100

    chore: add win check

--- js/app.js
+++ js/app.js
@@ -7,47 +7,80 @@ const addPhraseToDisplay = (phraseChars) => {
   const phraseList = document.querySelector('#phrase ul');
   
   phraseChars.forEach(phraseChar => {
-    phraseList.insertAdjacentHTML('beforeend', `<li class=${phraseChar !== ' ' ? 'letter' : null} />`)
+    phraseList.insertAdjacentHTML('beforeend', `<li class=${phraseChar !== ' ' ? 'letter' : null}>${phraseChar}</li>`)
   });
 };
 
 const checkLetter = (selectedLetter) => {
-  const selectedLetterValue = selectedLetter.textContext;
+  const selectedLetterValue = selectedLetter.textContent;
   const letterElements = document.querySelectorAll(`.letter`);
-  // store the seletedLetter somewhere
   
-  console.log('letterElements: ', letterElements);
+  let foundLetter = null;
   
   letterElements.forEach(letterElement => {
-    if (selectedLetterValue === letterElement.textContext) {
-      return letterElement;
-    } else {
-      return null;
+    if (selectedLetterValue === letterElement.textContent) {
+      letterElement.classList.add('show');
+      foundLetter = letterElement;
     }
-  })
+  });
+  
+  return foundLetter;
 };
 
-const app = () => {
-  const qwerty = document.getElementById('qwerty');
-  const phrase = document.getElementById('phrase');
-  const startButton = document.querySelector('.start a');
+const checkWin = (showLetters, letters, misses, overlayContainer) => {
+  if (misses >= 5) {
+    overlayContainer.style.display = 'flex';
+    overlayContainer.classList.add('lose');
+  }
   
-  let missed = 0;
+  if (showLetters !== 0 && letters !== 0) {
+    if (showLetters === letters) {
+      overlayContainer.style.display = 'flex';
+      overlayContainer.classList.add('win');
+    }
+  }
+}
+
+const app = () => {
+  let misses = 0;
+  let letterFound;
+  
+  const overlayContainer = document.querySelector('#overlay');
+  
+  const qwerty = document.querySelector('#qwerty');
+  const phrase = document.querySelector('#phrase');
+  const tries = document.querySelectorAll('.tries');
+  
+  const startButton = document.querySelector('.btn__reset');
   const phrases = ['run into the ground', 'catch the sun', 'over and over', 'pack away', 'loud and clear'];
   
   const selectedPhrase = getRandomPhraseAsArray(phrases);
   addPhraseToDisplay(selectedPhrase);
   
   startButton.addEventListener('click', () => {
-    const startOverlay = document.querySelector('#overlay');
-    startOverlay.style.display = 'none';
+    overlayContainer.classList.remove('start');
+    overlayContainer.style.display = 'none';
   });
   
   qwerty.addEventListener('click', (event) => {
     if (event.target.tagName === 'BUTTON') {
       event.target.classList.add('chosen');
       event.target.disabled = true;
-      checkLetter(event.target);
+      
+      letterFound = checkLetter(event.target);
+      
+      if (!letterFound) {
+        tries[misses].firstChild.src = 'images/lostHeart.png'
+        misses++;
+      }
+  
+      const showLetters = document.querySelectorAll('.show').length;
+      const letters = document.querySelectorAll('.letters').length;
+  
+      console.log('showLetters: ', showLetters);
+      console.log('letters: ', letters);
+      
+      checkWin(showLetters, letters, misses,  overlayContainer);
     }
   });
 };
